7fcbb180e650d5e36ba5aea320f6fafb72fe58d2
Samee24 <sameescouser24@gmail.com>
2016-05-27 01:15:44 -0400

    remove a lil cruft

--- wrapper.go
+++ wrapper.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-    fmt.Println("Hello World")
     resp, err := http.Get("https://api-ssl.bitly.com/v3/shorten?access_token=ACCESSTOKEN&longUrl=http%3A%2F%2Fgoogle.com%2F")
     if err != nil {
       fmt.Println("Error occurred")
