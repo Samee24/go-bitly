79be9d02e1f1e1ac0830b2c7ccd281c9aeb043c3
Samee24 <sameescouser24@gmail.com>
2016-07-07 20:46:48 -0700

    add some small comments

--- wrapper.go
+++ wrapper.go
@@ -12,29 +12,38 @@ const (
   baseApiURI   = "https://api-ssl.bitly.com"
   shortenEndPoint = "/v3/shorten"
 )
-
+// Instance to hold base-level response from the API
 type ShortenResponse struct {
     Status  string `json:"status_txt"`
+    // Nested field
     ShortenResponseData `json:"data"`
 }
 
+// Instance to hold data we actually care about from the API
 type ShortenResponseData struct {
     ShortUrl  string `json:"url"`
 }
 
 func main() {
     accessToken := ""
+    // Make sure we encode our URI
     longUrl := url.QueryEscape("http://samee.ninja")
 
-    var pojo ShortenResponse
     resp, err := http.Get(baseApiURI + shortenEndPoint + "?access_token=" + accessToken + "&longUrl=" + longUrl)
     if err != nil {
       fmt.Println("Error occurred")
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
+    // Print JSON response
     fmt.Println(string(body))
 
-    json.Unmarshal([]byte(string(body)), &pojo)
+    var respWrapper ShortenResponse
+    err = json.Unmarshal([]byte(string(body)), &respWrapper)
+    if err != nil {
+      fmt.Println("Error occurred")
+    }
+
+    // Print shortened link
     fmt.Println(pojo.ShortenResponseData.ShortUrl)
 }
