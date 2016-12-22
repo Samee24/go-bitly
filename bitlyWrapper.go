c8c1ab8a8829d3cabf4b21b3181f3beab045aa6c
Samee24 <sameescouser24@gmail.com>
2016-12-22 04:27:48 +0100

    more error handling + support for expand

--- bitlyWrapper.go
+++ bitlyWrapper.go
@@ -7,51 +7,100 @@ import (
 "net/url"
 "encoding/json"
 "os"
+"flag"
 )
 
 const (
-  baseApiURI   = "https://api-ssl.bitly.com"
-  shortenEndPoint = "/v3/shorten"
+  accessToken="ee3b33ba448f179ac37bdf4bfb888fa032f78d18"
+  baseApiURI="https://api-ssl.bitly.com"
+  shortenEndPoint="/v3/shorten"
+  expandEndPoint="/v3/expand"
+  urlShortenError="Please provide a url to shorten"
+  accessTokenError="Please provide an access token"
+  noFlagError="Please provide a flag to either shorten (-s) or expand (-e) your link."
+  tooManyFlagsError="Please provide only ONE flag to either shorten (-s) or expand (-e) your link."
 )
 // Instance to hold base-level response from the API
-type ShortenResponse struct {
+type Response struct {
     Status string `json:"status_txt"`
     // Nested field
-    ShortenResponseData `json:"data"`
+    ResponseData `json:"data"`
 }
 
 // Instance to hold data we actually care about from the API
-type ShortenResponseData struct {
-    ShortUrl  string `json:"url"`
+type ResponseData struct {
+    ShortUrl string `json:"url"`
+    ExpandedData []ExpandResponseData `json:"expand"`
+}
+
+// Instance to hold data we actually care about from the expand API call
+type ExpandResponseData struct {
+    LongUrl string `json:"long_url"`
+}
+
+func checkAccessToken(token string) {
+  if len(token) < 1 {
+      fmt.Println(accessTokenError)
+      os.Exit(1)
+    }
+}
+
+func checkArgs(args []string) {
+  if len(args) < 1 {
+      fmt.Println(urlShortenError)
+      os.Exit(1)
+    }
+}
+
+func checkFlagsAndAssignEndpoint(s bool, e bool, url string) string {
+  if !s && !e {
+      fmt.Println(noFlagError)
+      os.Exit(1)
+    }
+  if s && e {
+      fmt.Println(tooManyFlagsError)
+      os.Exit(1)
+  }
+  if s {
+    return baseApiURI + shortenEndPoint + "?access_token=" + accessToken + "&longUrl=" + url
+  } else {
+    return baseApiURI + expandEndPoint + "?access_token=" + accessToken + "&shortUrl=" + url
+  }
+
 }
 
 func main() {
-    accessToken := ""
-    if len(os.Args) < 2 {
-      fmt.Println("Please provide a url to shorten")
-      os.Exit(1)
-    }
+    shortenFlag := flag.Bool("s", false, "a bool for shortening")
+    expandFlag := flag.Bool("e", false, "a bool for expanding")
 
-    if len(accessToken) < 1 {
-      fmt.Println("Please enter an access token")
-      os.Exit(1)
-    }
+    checkArgs(os.Args)
+    checkAccessToken(accessToken)
+
+    flag.Parse()
+
     // Make sure we encode our URI
-    longUrl := url.QueryEscape(os.Args[1])
+    url := url.QueryEscape(os.Args[2])
+    currentEndpoint := checkFlagsAndAssignEndpoint(*shortenFlag, *expandFlag, url)
 
-    resp, err := http.Get(baseApiURI + shortenEndPoint + "?access_token=" + accessToken + "&longUrl=" + longUrl)
+    resp, err := http.Get(currentEndpoint)
+
     if err != nil {
       fmt.Printf("Error occurred\n %s", err)
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
+
     // Parse JSON
-    var respWrapper = new(ShortenResponse)
+    var respWrapper = new(Response)
     err = json.Unmarshal([]byte(body), &respWrapper)
     if err != nil {
       fmt.Printf("Error occurred: %s", respWrapper.Status)
     }
 
-    // Print shortened link
-    fmt.Println(respWrapper.ShortenResponseData.ShortUrl)
+    // Print shortened/expanded link
+    if (*shortenFlag) {
+       fmt.Println(respWrapper.ResponseData.ShortUrl)
+    } else {
+       fmt.Println(respWrapper.ResponseData.ExpandedData[0].LongUrl)
+    }
 }
